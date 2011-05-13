682032b40d798bdd7efcc4cba02486a0388c16d1
Koichi Onoue <onyoue@gmail.com>
2011-05-13 13:13:19 +0900

    Erlang Day1 2, 3

--- 7LangsIn7Weeks/Erlang/day1.erl
+++ 7LangsIn7Weeks/Erlang/day1.erl
@@ -1,5 +1,7 @@
 -module(day1).
 -export([word_count/1]).
+-export([count_to/1]).
+-export ([error_message/1]).
 
 word_count([32|B]) -> 1 + skip_space(B);
 word_count([A|B]) -> word_count(B);
@@ -8,3 +10,9 @@ word_count([]) -> 1.
 skip_space([32|B]) -> skip_space(B);
 skip_space([]) -> 0;
 skip_space(A) -> word_count(A).
+
+count_to(0) -> [0];
+count_to(N) -> count_to(N-1) ++ [N].
+
+error_message(success) -> success;
+error_message({error, Message}) -> "error: " ++ Message.
